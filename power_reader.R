e0e0674d90f9dec0dc16b814c697f1bff5c4b6b5
amoPisciculus <asoriano28@gmail.com>
2014-07-13 18:31:03 -0400

    update

--- power_reader.R
+++ power_reader.R
@@ -1,22 +1,10 @@
-# Function that reads and plots data from electric power consumption 
-# dataset 
-powerReader <- function(file) {
-  # 'file' is the name of the dataset file to be read 
-  # Return rows only from 2007-02-01 and 2007-02-02
+# Return rows only from 2007-02-01 and 2007-02-02
   
-  powerData <- read.csv(file)
+powerData <- read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'),header=F, sep=';')
   
-  # Convert Date and Time columns to appropriate
-  # Date and Time classes from character classes
-  powerData[,'Date'] <- as.Date(powerData[,'Date'])
-  powerData[,'Time'] <- strptime(powerData[,'Time'])
-  
-  # Set bounds for reading data
-  DATE_1 <- powerData$Date=='2007-02-01'
-  DATE_2 <- powerData$Date=='2007-02-02'
-  
-  # Enforce bounds on dataset
-  return
-  powerData[(DATE_1 | DATE_2),]
-}
+# Convert Date and Time columns to appropriate
+# Date and Time classes from character classes
+powerData[,1] <- as.Date(powerData[,1])
+powerData[,powerData$Time] <- strptime(powerData[,powerData$Time], format="%H:%M:%S")
+
 
